c67fd6507a7091e733833cce62fb9538e1f444f6
IsraelCastroGonzalez <israel.cagon@gmail.com>
2018-11-03 12:07:53 -0600

    First Commit

---
+++ Practices/README.md
@@ -0,0 +1,1 @@
+##Udemy Course "From Zero to Hero"
--- Practices/hello_world.py
+++ Practices/hello_world.py
@@ -1,2 +1,5 @@
 print("Hello World! We remember how to print!")
 print("Lets get this on GitHub!")
+a = 2
+b = 3
+print("the sum of a and b is: " + str(a + b))
